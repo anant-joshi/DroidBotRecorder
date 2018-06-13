08983d4d7a1932bdc2db226d5ea039beeb28b7c6
Anant Joshi <anantjoshi1324@gmail.com>
2018-06-13 11:07:22 +0530

    Add class to represent an input request

---
+++ app/src/main/java/org/honeynet/droidbotrecorder/InputRequest.java
@@ -0,0 +1,38 @@
+package org.honeynet.droidbotrecorder;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by anant on 11/6/18.
+ */
+
+public class InputRequest implements Comparable<InputRequest> {
+    private int x;
+    private int y;
+    private boolean isDown;
+    private double timestamp;
+
+    public InputRequest(int x, int y, boolean isDown, double timestamp) {
+        this.x = x;
+        this.y = y;
+        this.isDown = isDown;
+        this.timestamp = timestamp;
+    }
+
+    public boolean isDown() {
+        return isDown;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public int compareTo(@NonNull InputRequest ir) {
+        return (this.timestamp > ir.timestamp) ? 1 : -1;
+    }
+}
