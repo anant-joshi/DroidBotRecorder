f61a95568335efb9d5535d1957eec16c4ca7a810
Anant Joshi <anantjoshi1324@gmail.com>
2018-06-13 11:20:00 +0530

    Change MainActivity to run webserver and request handler.

--- app/src/main/java/org/honeynet/droidbotrecorder/MainActivity.java
+++ app/src/main/java/org/honeynet/droidbotrecorder/MainActivity.java
@@ -2,36 +2,29 @@ package org.honeynet.droidbotrecorder;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import org.honeynet.droidbotrecorder.injection.EventsInjector;
-import org.honeynet.droidbotrecorder.injection.InputDevice;
-
-import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestHandler handler;
+    WebServer webServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PriorityBlockingQueue<InputRequest> queue = new PriorityBlockingQueue<>();
+        this.webServer = new WebServer(queue, this);
+        this.handler = new RequestHandler(queue);
+        this.webServer.run();
+        this.handler.run();
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        EventsInjector injector = new EventsInjector();
-        Log.v("TestInject", "Trying to init");
-        injector.enableDebug(true);
-        int numDevices = injector.init();
-        Log.v("TestInject", "Number of devices: " + numDevices);
-        List<InputDevice> inputDevices = injector.inputDevices;
-        InputDevice testDevice = inputDevices.get(0);
-        Log.v("TestInject", "Trying to open the first device");
-        boolean isOpen = testDevice.open(true);
-        Log.v("TestInject", "The device is " + ((isOpen) ? "" : "not ") + "open");
-        Log.v("TestInject", "Trying to close device");
-        testDevice.close();
-        Log.v("TestInject", "Closed Device");
+    protected void onDestroy() {
+        super.onDestroy();
+        this.handler.close();
+        this.webServer.close();
     }
 }
