b31071fe37e0e99e2b82dd69ae529d09e6653a81
Anant Joshi <anantjoshi1324@gmail.com>
2018-07-09 17:02:26 +0530

    Add class to hold TouchEvents

---
+++ app/src/main/java/org/honeynet/droidbotrecorder/input/TouchEvent.java
@@ -0,0 +1,35 @@
+package org.honeynet.droidbotrecorder.input;
+
+
+public class TouchEvent {
+    private boolean isDown;
+    private int relX;
+    private int relY;
+    private long timeInMillis;
+
+
+
+
+    public TouchEvent(boolean isDown, int relX, int relY){
+        this.isDown = isDown;
+        this.relX = relX;
+        this.relY = relY;
+        this.timeInMillis = System.currentTimeMillis();
+    }
+
+    public boolean isDown() {
+        return isDown;
+    }
+
+    public int getRelX() {
+        return relX;
+    }
+
+    public int getRelY() {
+        return relY;
+    }
+
+    public long getTimeInMillis() {
+        return timeInMillis;
+    }
+}
